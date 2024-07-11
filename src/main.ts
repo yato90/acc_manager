9350a48ebb235ffdfcbf1e1f60a0dfafa8a17a19
yato90 <zahaoyam@yandex.ru>
2024-07-11 10:14:29 +0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(vuetify).mount('#app')
